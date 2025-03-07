586b8f3832bc680f6a0477556de0117d47b41576
Zybyte85 <cade.step01@gmail.com>
2025-03-07 11:05:55 -0700

    Fixed indentation, added flutter-tools.

--- lua/mvim/config/options.lua
+++ lua/mvim/config/options.lua
@@ -11,6 +11,7 @@ vim.opt.number = true -- Show line numbers
 vim.opt.relativenumber = true -- Show relative line numbers
 vim.opt.termguicolors = true -- Use colors in terminal
 vim.opt.copyindent = true -- Copy the indentation from the previous line
+vim.opt.expandtab = true -- Use spaces instead of tab characters
 
 -- Global options
 vim.g.mapleader = " " -- Set the leader key to space
